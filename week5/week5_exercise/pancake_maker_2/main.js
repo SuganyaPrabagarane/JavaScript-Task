623b85b0c3f104307971efe19904e57ec8a22eb3
Suganya Prabagarane <suganyaprabagarane@gmai.com>
2025-03-30 02:18:59 +0200

    update task

--- week5/week5_exercise/pancake_maker_2/main.js
+++ week5/week5_exercise/pancake_maker_2/main.js
@@ -8,35 +8,36 @@ const totalPriceBanner = document.querySelector('#totalPrice');
 const deliveryMethod = document.querySelector('.delivery:checked');
 
 
-const changeHandler = (event) =>{
-   
+const changeHandler = (event) => {
+
     const basePrice = parseFloat(pancakeType.selectedOptions[0].dataset.price);
 
-    const toppingsTotal =[...document.querySelectorAll('.topping:checked')].reduce((sum,topping)=> sum + parseFloat(topping.dataset.price), 0);
+    const toppingsTotal = [...document.querySelectorAll('.topping:checked')].reduce((sum, topping) => sum + parseFloat(topping.dataset.price), 0);
 
-    const extrasTotal =[...document.querySelectorAll('.extra:checked')].reduce((sum,extra)=> sum + parseFloat(extra.dataset.price), 0);
+    const extrasTotal = [...document.querySelectorAll('.extra:checked')].reduce((sum, extra) => sum + parseFloat(extra.dataset.price), 0);
 
     let deliveryPrice = 0;
-    if(deliveryMethod){
-         deliveryPrice = parseFloat(deliveryMethod.dataset.price);
+    if (deliveryMethod) {
+        deliveryPrice = parseFloat(deliveryMethod.dataset.price);
     }
 
-    let totalPrice = basePrice +toppingsTotal + extrasTotal + deliveryPrice;
+    let totalPrice = basePrice + toppingsTotal + extrasTotal + deliveryPrice;
 
-    totalPriceBanner.textContent =`${totalPrice}€`;
-    totalPriceDisplay.textContent =`${totalPrice}€`;
+    totalPriceBanner.textContent = `${totalPrice}€`;
+    totalPriceDisplay.textContent = `${totalPrice}€`;
 }
-form.addEventListener('change',changeHandler);
+form.addEventListener('change', changeHandler);
 
 
-const orderSummary =()=>{
+const orderSummary = () => {
 
+    const deliveryMethod = document.querySelector('.delivery:checked');
     const toppings = document.querySelectorAll('.topping:checked');
     const extras = document.querySelectorAll('.extra:checked');
     let toppingResult, extraResult;
     let topping = [];
     let extra = [];
-   
+
     for (let i = 0; i < toppings.length; i++) {
         if (toppings[i].checked) {
             toppingResult = toppings[i].value;
@@ -45,14 +46,14 @@ const orderSummary =()=>{
     }
     for (let i = 0; i < extras.length; i++) {
         if (extras[i].checked) {
-              extraResult = extras[i].value;
-              extra.push(extraResult);
+            extraResult = extras[i].value;
+            extra.push(extraResult);
         }
     }
-    summary.textContent = `Name: ${customerName.value}, Pancake Type: ${pancakeType.selectedOptions[0].textContent}, Selected Toppings: ${topping}, Added extra item : ${extra}, selected delivery: ${deliveryMethod.parentElement.textContent}` ;
+    summary.textContent = `Name: ${customerName.value}, Pancake Type: ${pancakeType.selectedOptions[0].textContent}, Selected Toppings: ${topping}, Added extra item : ${extra}, selected delivery: ${deliveryMethod.parentElement.textContent}`;
 }
 
-orderButton.addEventListener('click',orderSummary)
+orderButton.addEventListener('click', orderSummary)
 
 
 
